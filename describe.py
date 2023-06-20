940d0836e81e084e4758e4f4f25836eba5b66b3c
Clément Beaurain <cbeaurain@corp.free.fr>
2023-06-20 14:28:26 +0200

    describe ok

--- describe.py
+++ describe.py
@@ -1,29 +1,36 @@
 import sys
+import math
 import numpy as np
 import pandas as pd
 from ml_tools import isValidPath
+from ml_tools import count
+from ml_tools import mean
+from ml_tools import std
+from ml_tools import quantile
 
 def createIndexedDf(column):
-	print('col', column)
 	return pd.DataFrame(columns=column, index=["count", "mean", "std", "min", "25%", "50%", "75%", "max"])
 
 def compute(df, described):
-	for column in df:
+	for column in described.columns:
 		if df[column].dtype != "object":
-			described[column]["count"] = df[column].count()
-			described[column]["mean"] = df[column].mean()
-			described[column]["std"] = df[column].std()
-			described[column]["min"] = df[column].min()
-			described[column]["25%"] = df[column].quantile(.25)
-			described[column]["50%"] = df[column].quantile(.5)
-			described[column]["75%"] = df[column].quantile(.75)
-			described[column]["max"] = df[column].max()
+			sorted = df[column].sort_values()
+			new_data = [float(i) for i in sorted if not math.isnan(i)]
+			print('new', new_data)
+			described[column]["count"] = count(new_data)
+			described[column]["mean"] = mean(new_data)
+			described[column]["std"] = std(new_data, described[column]["mean"])
+			described[column]["min"] = new_data[0]
+			described[column]["25%"] = quantile(new_data, .25)
+			described[column]["50%"] = quantile(new_data, .5)
+			described[column]["75%"] = quantile(new_data, .75)
+			described[column]["max"] = new_data[len(new_data) - 1]
+	return described
 
 def describe(df):
-	described = createIndexedDf(df.select_dtypes([np.int64,np.float64]).columns)
-	compute(df, described)
-	print(described)
-
+	struct = createIndexedDf(df.dropna(how='all', axis=1).select_dtypes([np.int64,np.float64]).columns)
+	described = compute(df, struct)
+	print (described)
 
 if __name__ == "__main__":
 	if len(sys.argv) <= 1:
--- ml_tools.py
+++ ml_tools.py
@@ -5,6 +5,35 @@ import numpy as np
 from pathlib import Path
 import seaborn as sns
 
+def count(array):
+  return len(array)
+
+def mean(array):
+  return sum(array) / len(array)
+
+def min_(array):
+  min = array[0]
+  for i in range(len(array)):
+    if (array[i] < min):
+      min = array[i]
+  return min
+
+def quantile(array, q):
+  return array[int(len(array) * q)]
+
+def max_(array):
+  max = array[0]
+  for i in range(len(array)):
+    if (array[i] > max):
+      max = array[i]
+  return max
+
+def std(array, mean):
+  sum = 0
+  for i in array:
+    sum += (i - mean) ** 2
+  return (sum / len(array)) ** 0.5
+
 def isValidPath(filePath):
   if (path.isfile(filePath) == False):
     raise Exception('File does not exist')
