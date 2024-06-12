4394a17e0d429b40ea7bd1ef208c1da7d912ad62
cbeaurain <clementbeaurainpro@gmail.com>
2024-06-12 16:09:58 +0200

    feat(): log reg start

--- logreg_train.py
+++ logreg_train.py
@@ -8,6 +8,7 @@ import argparse
 
 ALPHA = 0.00001
 EPOCHS = 100
+HOUSE_CONVERTER = {"Slytherin": 1, "Gryffindor": 2, "Ravenclaw": 3, "Hufflepuff": 4}
 
 
 def compute_cost(x, y, slopes, intercept, *argv):
@@ -15,12 +16,12 @@ def compute_cost(x, y, slopes, intercept, *argv):
     Computes the cost over all examples
     Args:
         x : (ndarray Shape (m,n)) data, m examples by n features
-        y : (ndarray Shape (m,))  target value 
-        slopes : (ndarray Shape (n,))  values of parameters of the model      
+        y : (ndarray Shape (m,))  target value
+        slopes : (ndarray Shape (n,))  values of parameters of the model
         intercept : (scalar)              value of bias parameter of the model
       *argv : unused, for compatibility with regularized version below
     Returns:
-        total_cost : (scalar) cost 
+        total_cost : (scalar) cost
     """
 
     m, n = x.shape
@@ -32,30 +33,32 @@ def compute_cost(x, y, slopes, intercept, *argv):
             slopes_x_intercept = slopes_x_intercept + x[col][j] * slopes[j]
         slopes_x_intercept = slopes_x_intercept + intercept
         f_slopes_intercept_x = tools.sigmoid_(slopes_x_intercept)
-        loss = loss + (-1 * y[i] * np.log(f_slopes_intercept_x) -
-                       (1 - y[i]) * np.log(1 - f_slopes_intercept_x))
-    print('L', loss)
+        loss = loss + (
+            -1 * y[i] * np.log(f_slopes_intercept_x)
+            - (1 - y[i]) * np.log(1 - f_slopes_intercept_x)
+        )
+    print("L", loss)
     total_cost = loss / m
     return total_cost
 
 
 def compute_gradient(x, y, slopes, intercept, *argv):
     """
-    Computes the gradient for logistic regression 
+    Computes the gradient for logistic regression
 
     Args:
         x : (ndarray Shape (m,n)) data, m examples by n features
-        y : (ndarray Shape (m,))  target value 
-        slopes : (ndarray Shape (n,))  values of parameters of the model      
+        y : (ndarray Shape (m,))  target value
+        slopes : (ndarray Shape (n,))  values of parameters of the model
         intercept : (scalar)              value of bias parameter of the model
         *argv : unused, for compatibility with regularized version below
         Returns
-        dj_dslopes : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters slopes. 
-        dj_dintercept : (scalar)             The gradient of the cost w.r.t. the parameter intercept. 
+        dj_dslopes : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters slopes.
+        dj_dintercept : (scalar)             The gradient of the cost w.r.t. the parameter intercept.
     """
     m, n = x.shape
     dj_dslopes = np.zeros(slopes.shape)
-    dj_dintercept = 0.
+    dj_dintercept = 0.0
     for i in range(m):
         f_slopes_intercept_i = tools.sigmoid_(x.dot(slopes) + intercept)
         err_i = f_slopes_intercept_i[i] - y[i]
@@ -67,14 +70,16 @@ def compute_gradient(x, y, slopes, intercept, *argv):
     return dj_dintercept, dj_dslopes
 
 
-def gradient_descent(x, y, slopes, intercept, cost_function, gradient_function, lambda_):
+def gradient_descent(
+    x, y, slopes, intercept, cost_function, gradient_function, lambda_
+):
     """
-    Performs batch gradient descent to learn theta. Updates theta by taking 
+    Performs batch gradient descent to learn theta. Updates theta by taking
     num_iters gradient steps with learning rate alpha
 
     Args:
         x :    (ndarray Shape (m, n) data, m examples by n features
-        y :    (ndarray Shape (m,))  target value 
+        y :    (ndarray Shape (m,))  target value
         slopes : (ndarray Shape (n,))  Initial values of parameters of the model
         intercept : (scalar)              Initial value of parameter of the model
         cost_function :              function to compute cost
@@ -90,18 +95,17 @@ def gradient_descent(x, y, slopes, intercept, cost_function, gradient_function, 
 
     for _ in range(EPOCHS):
         cost = cost_function(x, y, slopes, intercept, lambda_)
-        dj_dintercept, dj_dslopes = gradient_function(
-            x, y, slopes, intercept, lambda_)
+        dj_dintercept, dj_dslopes = gradient_function(x, y, slopes, intercept, lambda_)
         intercept = intercept - ALPHA * dj_dintercept
         slopes = slopes - ALPHA * dj_dslopes
-        print('cost', cost)
-        print('intercept', intercept)
-        print('slopes', slopes)
+        print("cost", cost)
+        print("intercept", intercept)
+        print("slopes", slopes)
     return slopes, intercept, dj_dslopes, dj_dintercept
 
 
 def logistic_regression(x, y):
-    '''The logisticRegression function performs logistic regression on the given input data and plots the
+    """The logisticRegression function performs logistic regression on the given input data and plots the
     decision boundary.
 
     Parameters
@@ -115,24 +119,25 @@ def logistic_regression(x, y):
     each data point in the dataset. In the context of logistic regression, "y" typically contains binary
     values (0 or 1
 
-    '''
+    """
     _, n = x.shape
     intercept = 0
     slopes = np.zeros(n)
     slopes, intercept, _, _ = gradient_descent(
-        x, y, slopes, intercept, compute_cost, compute_gradient, 0)
+        x, y, slopes, intercept, compute_cost, compute_gradient, 0
+    )
     tools.plot_decision_boundary(slopes, intercept, x, y)
     # Set the y-axis label
-    plt.ylabel('Exam 2 score')
+    plt.ylabel("Exam 2 score")
     # Set the x-axis label
-    plt.xlabel('Exam 1 score')
+    plt.xlabel("Exam 1 score")
     plt.legend(loc="upper right")
     plt.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('csv_file', type=str, help='csv file')
+    parser.add_argument("csv_file", type=str, help="csv file")
     args = parser.parse_args()
     try:
         tools.is_valid_path(args.csv_file)
@@ -140,10 +145,12 @@ if __name__ == '__main__':
         print(e)
         sys.exit(e)
 
-    def transform_houses(x): return 1 if x == 'Slytherin' else 0
-    x, y = tools.load_data(
-        args.csv_file, "Hogwarts House", transform_houses)
-    logistic_regression(x, y)
+    def transform_houses(x):
+        return HOUSE_CONVERTER[x]
+
+    x, y = tools.load_data(args.csv_file, "Hogwarts House", transform_houses)
+    print(x, y)
+    # logistic_regression(x, y)
     # try:
     #     x, y = tools.load_data(
     #         args.csv_file, "Hogwarts House", transformHouses)
